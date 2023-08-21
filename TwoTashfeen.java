d5d0589de590ce8ebb909b6b84688da54280c098
damien25williams <123675813+damien25williams@users.noreply.github.com>
2023-08-21 15:33:37 -0500

    Completed

--- TwoTashfeen.java
+++ TwoTashfeen.java
@@ -1,1 +1,10 @@
-
+public class TwoTashfeen{
+  public static void main(String[] args){
+    int n = Integer.parseInt(args[0]);
+    int sum = 0;
+    for(int i = 1; i<= n; i++){
+      sum += i;
+    }
+    System.out.println(sum/n);
+  }
+}
